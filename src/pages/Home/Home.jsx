4ff04718fbd7691769bb0ec61984c2646330b6aa
aguslorca <aguslorca.12@gmail.com>
2024-02-06 17:46:17 -0300

    implemented companies-carousel

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import HeroHome from '../../components/Heros/HeroHome/HeroHome'
+import CompaniesCarousel from '../../components/CompaniesCarousel/CompaniesCarousel'
 
 const Home = () => {
   return (
     <Layout>
       <HeroHome/>
+      <CompaniesCarousel/>
+     
     </Layout>
   )
 }
