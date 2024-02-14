d9cebee607af9eecf3489a8272d09784adb49bff
aguslorca <aguslorca.12@gmail.com>
2024-02-14 17:12:56 -0300

    Added new ClientCarousel component. Remove CompaniesCarousel

---
+++ src/components/Carousels/CarouselClient/clients.js
@@ -0,0 +1,16 @@
+import Billabong from "../../../assets/images/empresas/billabong.png";
+import CeroEsTres from "../../../assets/images/empresas/cero-es-tres.png";
+import Farmacity from "../../../assets/images/empresas/farmacity.png";
+import GrupoAlas from "../../../assets/images/empresas/grupo-alas.png";
+import Newsan from "../../../assets/images/empresas/newsan.png";
+
+const clients = [
+    Billabong,
+    CeroEsTres,
+    Farmacity,
+    GrupoAlas,
+    Newsan,
+]
+
+
+export default clients;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import HeroHome from '../../components/Heros/HeroHome/HeroHome'
-import CompaniesCarousel from '../../components/CompaniesCarousel/CompaniesCarousel'
 import HomeCard from '../../components/Cards/HomeCard/HomeCard'
-import Slide from '../../components/Carousels/CarouselHome/Slide/Slide'
+import ClientContainer from '../../components/Carousels/CarouselClient/ClientContainer/ClientContainer'
 
 
 const Home = () => {
   return (
     <Layout>
       <HeroHome/>
-      <CompaniesCarousel/>
+      <ClientContainer/>
       <HomeCard/>
       <div style={{height:"500px"}}> 
       </div>
